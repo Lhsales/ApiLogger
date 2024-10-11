3cf90b553561b942f03b78d854f9b8224423d9dc
Lhsales <48722995+Lhsales@users.noreply.github.com>
2024-10-10 23:45:55 -0300

    Create middleware base

---
+++ ApiLogger.Serilog/ApiLoggerExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ApiLogger.Serilog
+{
+    public static class ApiLoggerExtension
+    {
+        public static IApplicationBuilder UseApiLoggerSerilogMiddleware(this IApplicationBuilder builder) 
+        {
+            return builder.UseMiddleware<ApiLoggerMiddleware>();
+        }
+    }
+}
---
+++ ApiLogger.Serilog/ApiLoggerMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ApiLogger.Serilog
+{
+    internal class ApiLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ApiLoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {            
+            await _next(context);
+        }
+
+    }
+}
