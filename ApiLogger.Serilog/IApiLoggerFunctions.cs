971760f3c46d654f528cbd27321ed59c1eda5b8b
Lhsales <48722995+Lhsales@users.noreply.github.com>
2024-10-11 23:00:39 -0300

    Create property factory

---
+++ ApiLogger.Serilog/IApiLoggerFunctions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ApiLogger.Serilog
+{
+    public interface IApiLoggerFunctions
+    {
+        Task CreateRequest(HttpRequest request);
+        Task CreateResponse(HttpContext context, RequestDelegate next);
+        void WriteLog(HttpContext context);
+    }
+}
